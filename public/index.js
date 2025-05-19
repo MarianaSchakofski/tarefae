06789ece5a24ffb286cc7c6e35c207fb1902d52b
MarianaSchakofski <159267225+MarianaSchakofski@users.noreply.github.com>
2025-05-19 11:36:57 -0300

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,12 @@
+function funcao(){
+    let a= document.getElementById("num1").value;
+    let b= document.getElementById("num2").value;
+
+    let resposta = fetch('/soma',{
+        method: 'post',
+        headers:{'content-type': 'application/json'},
+        body: JSON.stringify({a,b})
+    });
+    let valor= resposta.json();
+    document.getElementById("resultado").innerHTML=valor;
+}
